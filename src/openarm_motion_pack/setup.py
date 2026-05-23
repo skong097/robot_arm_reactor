753bb6f8de75b63c5afe2bb57b14837c6053a955
skong097 <skong097@gmail.com>
2026-05-24 06:37:20 +0900

    feat(openarm_motion_pack): 패키지 골격 신규 (package.xml + setup.py + 빈 init)
    OpenArm v10 bimanual (16 DOF) motion pack. trajectories.py + motions.py +
    launch + test 는 Task 6~8 에서. openarm_description/openarm_bringup/openarm_hardware
    exec_depend.

---
+++ src/openarm_motion_pack/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'openarm_motion_pack'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gjkong',
+    maintainer_email='skong097@gmail.com',
+    description='OpenArm v10 bimanual (16 DOF) motion pack for arm_reactor_core.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
